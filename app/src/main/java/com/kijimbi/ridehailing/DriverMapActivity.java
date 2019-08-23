4ef98404179ae852436c13101b0c2457a848ec8a
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2019-08-22 20:48:35 -0600

    Implement login mechanism for driver

---
+++ app/src/main/java/com/kijimbi/ridehailing/DriverMapActivity.java
@@ -0,0 +1,14 @@
+package com.kijimbi.ridehailing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_map);
+    }
+}
