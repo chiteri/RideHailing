f1313f0e95a1e2fa5aac48c9f76b5cb70b47cb25
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2019-08-23 09:40:51 -0600

    Implement login mechanism for Customer

---
+++ app/src/main/java/com/kijimbi/ridehailing/CustomerMapActivity.java
@@ -0,0 +1,14 @@
+package com.kijimbi.ridehailing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_map);
+    }
+}
