ce3f860bcf69be64e29389d5fb1721e627aa87dd
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2019-09-22 19:24:36 -0600

    Set up page for showing details of a single ride's history

--- app/src/main/java/com/kijimbi/ridehailing/historyRecyclerView/HistoryViewHolders.java
+++ app/src/main/java/com/kijimbi/ridehailing/historyRecyclerView/HistoryViewHolders.java
@@ -1,11 +1,14 @@
 package com.kijimbi.ridehailing.historyRecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kijimbi.ridehailing.HistorySingleActivity;
 import com.kijimbi.ridehailing.R;
 
 public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -22,6 +25,10 @@ public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.
 
     @Override
     public void onClick(View v) {
-
+        Intent intent = new Intent(v.getContext(), HistorySingleActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("rideId", rideId.getText().toString());
+        intent.putExtras(bundle);
+        v.getContext().startActivity(intent);
     }
 }
